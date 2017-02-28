247b0864d8c09e73173b92289ec95f1f7c31df5d
Wendi Zhang <zhan1686@purdue.edu>
2017-02-28 17:14:02 -0500

    Updated drawable source files

---
+++ AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/Activity/homepage.java
@@ -0,0 +1,15 @@
+package com.example.zuoyangding.aroundme.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.zuoyangding.aroundme.R;
+
+public class homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
