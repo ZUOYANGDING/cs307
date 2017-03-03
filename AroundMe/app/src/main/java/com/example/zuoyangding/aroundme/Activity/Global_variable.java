1cab7fea19929b5046e8bc8c1727d692992331f9
siyujiang7 <siyujiang@pal-nat186-94-132.itap.purdue.edu>
2017-03-03 10:30:11 -0500

    mixed version

---
+++ AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/Activity/Global_variable.java
@@ -0,0 +1,52 @@
+package com.example.zuoyangding.aroundme.Activity;
+
+import android.app.Application;
+import android.widget.ImageView;
+
+/**
+ * Created by siyujiang on 3/3/17.
+ */
+
+public class Global_variable extends Application{
+        private String user_id;
+        private String user_name;
+        private String birthday;
+        private String introduction;
+        private ImageView profile_pic;
+    public Global_variable(){
+        user_id = "undefined";
+        user_name = "307";
+        birthday = "0 0 0";
+        introduction = "This is a default profile";
+    }
+    public void setUser_id(String user_id){
+        this.user_id = user_id;
+    }
+    public void setUser_name(String user_name){
+        this.user_name = user_name;
+    }
+    public void setBirthday(String birthday){
+        this.birthday = birthday;
+    }
+    public void setIntroduction(String introduction){
+        this.introduction = introduction;
+    }
+    public void setProfile_pic(ImageView profile_pic){
+        this.profile_pic = profile_pic;
+    }
+    public String getUser_id(){
+        return this.user_id;
+    }
+    public String getUser_name(){
+        return this.user_name;
+    }
+    public String getBirthday(){
+        return this.birthday;
+    }
+    public String getIntroduction(){
+        return this.introduction;
+    }
+    public ImageView getProfile_pic(){
+        return this.profile_pic;
+    }
+}
--- AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/Activity/LandingActivity.java
+++ AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/Activity/LandingActivity.java
@@ -2,19 +2,14 @@ package com.example.zuoyangding.aroundme.Activity;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.zuoyangding.aroundme.R;
-
-import java.io.File;
 
 import static com.example.zuoyangding.aroundme.Activity.editLandingActivity.Birthday;
 import static com.example.zuoyangding.aroundme.Activity.editLandingActivity.Nickname;
@@ -36,16 +31,18 @@ public class LandingActivity extends AppCompatActivity implements View.OnClickLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
-
+        Global_variable global_variable = (Global_variable)getApplicationContext();
+        //global_variable.getUser_name();
+        //global_variable.getIntroduction();
         landing_Edit = (Button) findViewById(R.id.landing_Edit);
 
         landing_Nickname = (TextView) findViewById(R.id.landing_Nickname);
         landing_Birthday = (TextView) findViewById(R.id.landing_Birthday);
         landing_info = (TextView) findViewById(R.id.landing_intro);
 
-        landing_Nickname.setText(Nickname);
-        landing_Birthday.setText(Birthday);
-        landing_info.setText(info);
+        landing_Nickname.setText(global_variable.getUser_name());
+        landing_Birthday.setText(global_variable.getBirthday());
+        landing_info.setText(global_variable.getIntroduction());
 
         landing_Edit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
