b1c7340edba3ff99f9cc4d77192e6cb68b90c79f
Wendi Zhang <zhan1686@purdue.edu>
2017-03-03 17:05:45 -0500

    Updated homepage and chat group layout Added favorites page

---
+++ AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/Activity/favorites.java
@@ -0,0 +1,15 @@
+package com.example.zuoyangding.aroundme.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.zuoyangding.aroundme.R;
+
+public class favorites extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+    }
+}
