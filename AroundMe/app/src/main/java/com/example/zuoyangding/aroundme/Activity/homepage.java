b21bb77cfe5355557dc6d8c2cebeb1aac0e2f975
Kenny <zheng323@purdue.edu>
2017-03-08 01:18:19 -0500

    dataSnapshot error

--- AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/Activity/homepage.java
+++ AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/Activity/homepage.java
@@ -1,25 +1,71 @@
 package com.example.zuoyangding.aroundme.Activity;
 
+import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.example.zuoyangding.aroundme.DataModels.GroupClass;
 import com.example.zuoyangding.aroundme.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class homepage extends AppCompatActivity {
 
     ImageButton addGroupButton;
     ImageButton profileButton;
 
+    ListView groupList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
+        final ArrayList<GroupClass> nameList = new ArrayList<GroupClass>();
+
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference databaseReference = database.getReference();
+//        databaseReference.child("Group").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+//                Log.d("test1", "" + children);
+//                for(DataSnapshot child : children) {
+//                    GroupClass value = child.getValue(GroupClass.class);
+//                    nameList.add(value);
+//                    Log.d("test", " " + value);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+
+
         addGroupButton = (ImageButton) findViewById(R.id.addGroupButton);
         profileButton = (ImageButton) findViewById(R.id.profileButton);
+
+        groupList = (ListView) findViewById(R.id.group_list);
 
         addGroupButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -34,5 +80,8 @@ public class homepage extends AppCompatActivity {
                 homepage.this.startActivity(i);
             }
         });
+
+        ListAdapter adapter = new ArrayAdapter<GroupClass>(this, android.R.layout.simple_list_item_1, nameList);
+        groupList.setAdapter(adapter);
     }
 }
