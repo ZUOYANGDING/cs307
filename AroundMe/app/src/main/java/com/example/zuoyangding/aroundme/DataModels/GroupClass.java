a854997ba7db26ee155ad01ec2ba5cfbaea9a023
siyujiang7 <siyujiang@pal-nat186-86-110.itap.purdue.edu>
2017-03-16 07:55:32 -0400

    getting location function completed v1

--- AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/DataModels/GroupClass.java
+++ AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/DataModels/GroupClass.java
@@ -1,5 +1,7 @@
 package com.example.zuoyangding.aroundme.DataModels;
 
+import android.location.Location;
+
 import java.util.ArrayList;
 
 /**
@@ -12,13 +14,18 @@ public class GroupClass {
     public String key;
     public String topic;
     public long date;
+    public boolean is_permanent;
+    public Location mlocation;
     public ArrayList<String> member_ids;
-    public GroupClass(String groupName, String key, String topic, long date, ArrayList<String> member_ids) {
+
+    public GroupClass(String groupName, String key, String topic, long date, ArrayList<String> member_ids, Location mlocation, Boolean is_permanent) {
         this.groupName = groupName;
         this.key = key;
         this.topic = topic;
         this.date = date;
         this.member_ids = member_ids;
+        this.is_permanent = is_permanent;
+        this.mlocation = mlocation;
     }
 
 }
