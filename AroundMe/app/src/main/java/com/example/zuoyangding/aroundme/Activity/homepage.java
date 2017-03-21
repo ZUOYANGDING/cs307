9094063350b83c64fd955e548d1305b7aaebb973
Wendi Zhang <zhan1686@purdue.edu>
2017-03-21 17:26:41 -0400

    updated landing_activity

--- AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/Activity/homepage.java
+++ AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/Activity/homepage.java
@@ -4,23 +4,74 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 import com.example.zuoyangding.aroundme.R;
 
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class homepage extends AppCompatActivity {
 
-    ImageButton addGroupButton;
-    ImageButton profileButton;
+
+    private ListView listView;
+    private ImageButton addGroupButton;
+    private ImageButton profileButton;
+//    private Button logout;
+    private FirebaseAuth firebaseAuth;
+    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-
+        listView = (ListView)findViewById(R.id.group_list);
+        firebaseAuth = FirebaseAuth.getInstance();
+//        logout = (Button) findViewById(R.id.logout_bt);
+        userId = firebaseAuth.getCurrentUser().getUid();
         addGroupButton = (ImageButton) findViewById(R.id.addGroupButton);
         profileButton = (ImageButton) findViewById(R.id.profileButton);
 
+        final Global_variable global_variable = (Global_variable)getApplicationContext();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users").child(userId).child("groupIDs");
+        if (ref != null) {
+            FirebaseListAdapter<String> firebaseListAdapter = new FirebaseListAdapter<String>(this,
+                    String.class,
+                    R.layout.list_element,
+                    ref) {
+                @Override
+                protected void populateView(View v, final String model, int position) {
+                    final View v1 = v;
+                    DatabaseReference mref = FirebaseDatabase.getInstance().getReference().child("Group");
+
+                    mref.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            TextView t = (TextView) v1.findViewById(R.id.item1);
+                            t.setText(dataSnapshot.child(model).child("groupName").getValue().toString());
+                            TextView subt = (TextView) v1.findViewById(R.id.sub_item1);
+                            subt.setText(dataSnapshot.child(model).child("topic").getValue().toString());
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            System.out.println("fetch group failed");
+                        }
+                    });
+
+                }
+            };
+            listView.setAdapter(firebaseListAdapter);
+        }
         addGroupButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent i=new Intent(homepage.this, add_group.class);
@@ -34,5 +85,15 @@ public class homepage extends AppCompatActivity {
                 homepage.this.startActivity(i);
             }
         });
+
+//        logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                firebaseAuth.signOut();
+//                finish();
+//                Intent login = new Intent(homepage.this, LoginActivity.class);
+//                startActivity(login);
+//            }
+//        });
     }
 }
--- AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/DataModels/User.java
+++ AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/DataModels/User.java
@@ -1,5 +1,6 @@
 package com.example.zuoyangding.aroundme.DataModels;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,11 +13,15 @@ public class User {
     private String nickName;
     private String introduction;
     private String  birthday;
-    private List<String> groupIDs;
+    private ArrayList<String> groupIDs;
+    private String email;
+    private String password;
     //private String profile_image;
     //private String profile;
 
-    public User(String userID, String googleAccount, String nickName, String birthday, String introduction, List<String> groupIDs) {
+    public User(String userID, String email, String password, String googleAccount, String nickName, String birthday, String introduction, ArrayList<String> groupIDs) {
+        this.email = email;
+        this.password = password;
         this.userID = userID;
         this.nickName = nickName;
         this.birthday = birthday;
@@ -25,34 +30,38 @@ public class User {
         this.nickName = nickName;
         this.groupIDs = groupIDs;
     }
-
     public String getUserID() {
         return this.userID;
     }
     public void setUserID(String userID) {
         this.userID =userID;
     }
-
     public String getGoogleAccount() {
         return this.googleAccount;
     }
     public void setGoogleAccount(String googleAccount){
         this.googleAccount = googleAccount;
     }
-
+    public void setNickName(String nickName) {this.nickName = nickName;}
     public String getNickName(){
         return nickName;
     }
     public String getBirthday(){
         return birthday;
     }
+    public void setBirthday(String birthday) {this.birthday = birthday;}
+    public void setIntroduction (String introduction) {this.introduction = introduction;}
     public String getIntroduction(){
         return introduction;
     }
     public List<String> getGroupIDs() {
         return this.groupIDs;
     }
-    public void setGroupIDs(List<String> groupIDs) {
+    public void setGroupIDs(ArrayList<String> groupIDs) {
         this.groupIDs = groupIDs;
     }
+    public void setEmail(String email) {this.email = email;}
+    public String getEmail() {return this.email;}
+    public void setPassword(String password) {this.password = password;}
+    public String getPassword() {return this.password;}
 }
