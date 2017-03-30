ed438a6a459c7753518392bb426525a6ce9860a3
Kenny <zheng323@purdue.edu>
2017-03-30 03:29:27 -0400

    chat

--- AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/Activity/group_chat.java
+++ AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/Activity/group_chat.java
@@ -1,67 +1,67 @@
-package com.example.zuoyangding.aroundme.Activity;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import com.example.zuoyangding.aroundme.DataModels.chatMessage;
-import com.example.zuoyangding.aroundme.R;
-import com.firebase.ui.database.FirebaseListAdapter;
-import com.google.firebase.database.FirebaseDatabase;
-
-
-public class group_chat extends AppCompatActivity {
-
-    private FirebaseListAdapter<chatMessage> adapter;
-
-    ImageButton sendMessage;
-
-    TextView enterMessage;
-    TextView messageText;
-    TextView messageName;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_chat);
-
-        Log.d("test1", "test1");
-        sendMessage = (ImageButton) findViewById(R.id.imageButton3);
-        enterMessage = (TextView)findViewById(R.id.enterMessage);
-
-        sendMessage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                FirebaseDatabase.getInstance().getReference().push().setValue(new chatMessage(enterMessage.getText().toString(),
-//                        FirebaseAuth.getInstance().getCurrentUser().getEmail()));
-//                enterMessage.setText("");
-//                enterMessage.requestFocus();
-
-                Log.d("test", "test");
-                display();
-            }
-        });
-    }
-
-    private void display() {
-        final ListView messages = (ListView) findViewById(R.id.chat_messages);
-
-        adapter = new FirebaseListAdapter<chatMessage>(group_chat.this, chatMessage.class, R.layout.activity_display_messages, FirebaseDatabase.getInstance().getReference()) {
-            @Override
-            protected void populateView(View v, chatMessage model, int position) {
-
-                messageText = (TextView) findViewById(R.id.display_messages_message);
-                messageName = (TextView) findViewById(R.id.display_messages_User);
-
-                messageText.setText(model.getMessage());
-                messageName.setText(model.getNickname());
-
-            }
-        };
-        messages.setAdapter(adapter);
-    }
-}
+//package com.example.zuoyangding.aroundme.Activity;
+//
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.view.View;
+//import android.widget.ImageButton;
+//import android.widget.ListView;
+//import android.widget.TextView;
+//
+//import com.example.zuoyangding.aroundme.DataModels.chatMessage;
+//import com.example.zuoyangding.aroundme.R;
+//import com.firebase.ui.database.FirebaseListAdapter;
+//import com.google.firebase.database.FirebaseDatabase;
+//
+//
+//public class group_chat extends AppCompatActivity {
+//
+//    private FirebaseListAdapter<chatMessage> adapter;
+//
+//    ImageButton sendMessage;
+//
+//    TextView enterMessage;
+//    TextView messageText;
+//    TextView messageNickname;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_group_chat);
+//
+//        Log.d("test1", "test1");
+//        sendMessage = (ImageButton) findViewById(R.id.send_message);
+//        enterMessage = (TextView)findViewById(R.id.group_chat_enterMessage);
+//
+//        sendMessage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                FirebaseDatabase.getInstance().getReference().push().setValue(new chatMessage(enterMessage.getText().toString(),
+////                        FirebaseAuth.getInstance().getCurrentUser().getEmail()));
+////                enterMessage.setText("");
+////                enterMessage.requestFocus();
+//
+//                Log.d("test", "test");
+//                display();
+//            }
+//        });
+//    }
+//
+//    private void display() {
+//        final ListView messages = (ListView) findViewById(R.id.chat_messages);
+//
+//        adapter = new FirebaseListAdapter<chatMessage>(group_chat.this, chatMessage.class, R.layout.activity_group_chat_list, FirebaseDatabase.getInstance().getReference()) {
+//            @Override
+//            protected void populateView(View v, chatMessage model, int position) {
+//
+//                messageNickname = (TextView) v.findViewById(R.id.group_chat_nickname);
+//                messageText = (TextView) v.findViewById(R.id.display_messages_User);
+//
+//                messageText.setText(model.getMessage());
+//                messageNickname.setText(model.getNickname());
+//
+//            }
+//        };
+//        messages.setAdapter(adapter);
+//    }
+//}
--- AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/Activity/homepage.java
+++ AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/Activity/homepage.java
@@ -1,16 +1,22 @@
 package com.example.zuoyangding.aroundme.Activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.AdapterView;
 
 import com.example.zuoyangding.aroundme.R;
 
@@ -36,6 +42,12 @@ public class homepage extends AppCompatActivity {
     static FirebaseListAdapter<String> firebaseListAdapter;
     private DatabaseReference ref;
     private FirebaseAuth.AuthStateListener mAuthListener;
+
+    //image module by Frank Hu
+    private String landing_imgStr;
+
+    public static String globalGroupName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,7 +75,56 @@ public class homepage extends AppCompatActivity {
         addGroupButton = (ImageButton) findViewById(R.id.addGroupButton);
         profileButton = (ImageButton) findViewById(R.id.profileButton);
 
+//        //image module by Frank Hu
+//        DatabaseReference mref = FirebaseDatabase.getInstance().getReference().child("Users");
+//        mref.child(userId).addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//
+//                if (dataSnapshot.child("imgStr").getValue() != null) {
+//                    landing_imgStr = (String) dataSnapshot.child("imgStr").getValue();
+//
+//                    //Bitmap way
+//                    byte[] imageByte = Base64.decode(landing_imgStr,Base64.DEFAULT);
+//                    Bitmap bitmap = BitmapFactory.decodeByteArray(imageByte,0,imageByte.length);
+//                    profileButton.setImageBitmap(bitmap);
+//
+//                    //Uri way
+//                    //Uri imgUri = Uri.parse(landing_imgStr);
+//                    //landing_iv.setImageURI(imgUri);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
         final Global_variable global_variable = (Global_variable)getApplicationContext();
+        DatabaseReference mref = FirebaseDatabase.getInstance().getReference().child("Users");
+        mref.child(global_variable.getUser_id()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //image module by Frank Hu
+                if (dataSnapshot.child("imgStr").getValue() != null) {
+                    landing_imgStr = (String) dataSnapshot.child("imgStr").getValue();
+
+                    //Bitmap way
+                    byte[] imageByte = Base64.decode(landing_imgStr, Base64.DEFAULT);
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(imageByte, 0, imageByte.length);
+                    profileButton.setImageBitmap(bitmap);
+
+                    //Uri way
+                    //Uri imgUri = Uri.parse(landing_imgStr);
+                    //landing_iv.setImageURI(imgUri);
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
         ref = FirebaseDatabase.getInstance().getReference().child("Users").child(global_variable.getUser_id()).child("groupIDs");
         if (ref != null) {
             firebaseListAdapter = new FirebaseListAdapter<String>(this,
@@ -74,12 +135,17 @@ public class homepage extends AppCompatActivity {
                 protected void populateView(View v, final String model, int position) {
                     final View v1 = v;
                     DatabaseReference mref = FirebaseDatabase.getInstance().getReference().child("Group");
+                    final View vi = v;
+
 
                     mref.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
+                            vi.setTag(dataSnapshot.child(model).child("key").getValue().toString());
+
                             TextView t = (TextView) v1.findViewById(R.id.item1);
                             t.setText(dataSnapshot.child(model).child("groupName").getValue().toString());
+
                             TextView subt = (TextView) v1.findViewById(R.id.sub_item1);
                             subt.setText(dataSnapshot.child(model).child("topic").getValue().toString());
                         }
@@ -107,6 +173,18 @@ public class homepage extends AppCompatActivity {
                 homepage.this.startActivity(i);
             }
         });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+             @Override
+             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                               View v = view;
+                                String gid = v.getTag().toString();
+                                //System.out.println(uid);
+                                Intent i = new Intent(homepage.this, display_messageActivity.class);
+                                i.putExtra("groupid",gid);
+                                startActivity(i);
+                            }
+         });
 
 //        logout.setOnClickListener(new View.OnClickListener() {
 //            @Override
