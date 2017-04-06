0e1093069e40af7bae746b81241c8b8cf3ff0132
Frank Hu <hu353@purdue.edu>
2017-04-06 06:37:07 -0400

    Add homepage avatar function back
    update homepage user's avatar from firebase

--- AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/Activity/homepage.java
+++ AroundMe/app/src/main/java/com/example/zuoyangding/aroundme/Activity/homepage.java
@@ -2,8 +2,11 @@ package com.example.zuoyangding.aroundme.Activity;
 
 import android.content.Intent;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
@@ -72,6 +75,31 @@ public class homepage extends AppCompatActivity {
 
         final Global_variable global_variable = (Global_variable)getApplicationContext();
         //ArrayList<String> group_ids;
+
+        ////image module by Frank Hu (update homepage user's avatar from firebase)
+        DatabaseReference mref = FirebaseDatabase.getInstance().getReference().child("Users");
+        mref.child(global_variable.getUser_id()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //image module by Frank Hu
+                if (dataSnapshot.child("imgStr").getValue() != null) {
+                    landing_imgStr = (String) dataSnapshot.child("imgStr").getValue();
+
+                    //Bitmap way
+                    byte[] imageByte = Base64.decode(landing_imgStr, Base64.DEFAULT);
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(imageByte, 0, imageByte.length);
+                    profileButton.setImageBitmap(bitmap);
+
+                    //Uri way
+                    //Uri imgUri = Uri.parse(landing_imgStr);
+                    //landing_iv.setImageURI(imgUri);
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         ref = FirebaseDatabase.getInstance().getReference().child("Users").child(global_variable.getUser_id()).child("groupIDs");
         ref.addValueEventListener(new ValueEventListener() {
